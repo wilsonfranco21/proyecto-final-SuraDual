0f79e76664ccd49b32677aa1aacb3a7df765ea01
Christianramos <cramosari@cesde.net>
2024-07-09 15:39:04 -0500

    version proyecto

---
+++ src/controllers/controlador.js
@@ -0,0 +1,22 @@
+let boton=document.getElementById("botonchat")
+
+let cajaChat=document.getElementById("cajaChat")
+
+function cambiarCaja(){
+    let boton=document.getElementById("botonchat")
+
+    let cajaChat=document.getElementById("cajaChat")
+
+    if (cajaChat.style.display=="none"|| cajaChat.style.display==""){
+        cajaChat.style.display="block"
+
+    }else{
+        cajaChat.style.display="none"
+
+    }
+}
+
+
+
+
+    
